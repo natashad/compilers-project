4ee0400d3f03dbfc08c70f2745adfe8dd5074191
Showzeb Ali <showzeb110@gmail.com>
2014-02-27 22:34:19 -0800

    UnaryMinusExpn semantic check

--- A3/src/compiler488/ast/expn/UnaryMinusExpn.java
+++ A3/src/compiler488/ast/expn/UnaryMinusExpn.java
@@ -1,7 +1,9 @@
 package compiler488.ast.expn;
 
+import compiler488.ast.type.BooleanType;
 import compiler488.ast.type.IntegerType;
 import compiler488.ast.type.Type;
+import compiler488.semantics.SemanticError;
 import compiler488.semantics.Semantics;
 
 /**
@@ -23,6 +25,12 @@ public class UnaryMinusExpn extends UnaryExpn {
 		//Set type to Integer.
 		this.setType(new IntegerType());
 		
-		//TODO: Error checking if both expr type are Integer.
+		//S31
+		//Check the type of expression is Integer
+		if (this.getOperand().getType() == null || !(this.getOperand().getType() instanceof IntegerType)) {
+			SemanticError error = new SemanticError("Expression " + this.getOperand() + " is not Integer type", getLineNumber());
+			semantics.errorList.add(error);
+		}
 	}
+
 }
